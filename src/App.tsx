1333fb533c55bbae90e6969ee8d3278dccbcbd90
Dan Rozén <dan.rozen@bjornlunden.com>
2025-07-26 18:08:30 +0200

    Initial commit: Add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>weather-app</h1>
+        <p>
+          Built with Lovable - Your app is now live! 🚀
+        </p>
+        <p>
+          Edit <code>src/App.tsx</code> and save to see changes.
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
